4f59c649a69734fd2c10fafcb970e6f54bb667a6
cher-nov <blackdoomer@yandex.ru>
2017-01-28 03:09:59 +1000

    Change bunch of macros and comments.

--- linmath.h
+++ linmath.h
@@ -3,52 +3,51 @@
 
 #include <math.h>
 
-#define LINMATH_H_NEAR_ZERO (1e-6)
+#ifndef LINMATH_H_NEAR_ZERO
+	#define LINMATH_H_NEAR_ZERO (1e-6)
+#endif
 
 /*
-* M4E - access mat4x4 element
-* POS - positive if not row-major, otherwise negative
-* NEG - same as POS, but vice versa
+  Next macros are provided to implement optional row-major mode for matrices:
+  __LH_M4E - access mat4x4 element
+  __LH_POS - positive if not row-major, otherwise negative
+  __LH_NEG - same as __LH_POS, but vice versa
 */
 
-#if defined(M4E) || defined(POS) || defined(NEG)
-  #error linmath.h: tech macro redefined
-#endif
-
 #ifndef LINMATH_H_ROW_MAJOR
-	#define M4E(m,c,r) (m[c][r])
-	#define POS(x) (x)
-	#define NEG(x) (-x)
+	#define __LH_M4E(m,c,r) (m[c][r])
+	#define __LH_POS(x) (x)
+	#define __LH_NEG(x) (-x)
 #else
-	#define M4E(m,c,r) (m[r][c])
-	#define POS(x) (-x)
-	#define NEG(x) (x)
+	#define __LH_M4E(m,c,r) (m[r][c])
+	#define __LH_POS(x) (-x)
+	#define __LH_NEG(x) (x)
 #endif
 
 /*
-* Union vec#_t types support unnamed vector elements access when
-* supported by compiler - that is, for example,
-* 	vec#_t.x = 1.0f;
-* instead of
-* 	vec#_t.i.x = 1.0f;
-* Note that the named syntax is provided in any way for those who
-* doesn't want to use less portable unnamed syntax.
+  Union vec#_t types support unnamed vector elements access when
+  supported by compiler - that is, for example,
+  	vec#_t.x = 1.0f;
+  instead of
+  	vec#_t.i.x = 1.0f;
+  Note that the named syntax (with ".i") is provided in any way for those who
+  don't want to use less portable unnamed syntax.
 */
 
 /* Unnamed structs - since C11; as compiler extensions in older standards. */
-#if !defined(LINMATH_H_VECTOR_UNNAMED) && defined(__STDC_VERSION__)
-	#if (__STDC_VERSION__ >= 201112L) || \
-	  ( defined(__GNUC__) && !defined(__STRICT_ANSI__) )
-		#define LINMATH_H_VECTOR_UNNAMED 1
+#if !defined(LINMATH_H_NO_UNNAMED_STRUCTS) && defined(__STDC_VERSION__)
+	#if (__STDC_VERSION__ < 201112L) && \
+	  ( !defined(__GNUC__) || defined(__STRICT_ANSI__) )
+		#define LINMATH_H_NO_UNNAMED_STRUCTS
 	#endif
 #endif
 
-#ifdef LINMATH_H_VECTOR_UNNAMED
-	#define _LINMATH_H_VECTOR_STRUCT(...) \
+#ifndef LINMATH_H_NO_UNNAMED_STRUCTS
+	#define __LH_VECTOR_STRUCT(...) \
 		struct {float __VA_ARGS__;}; \
 		struct {float __VA_ARGS__;} i
 #else
-	#define _LINMATH_H_VECTOR_STRUCT(...) \
+	#define __LH_VECTOR_STRUCT(...) \
 		struct {float __VA_ARGS__;} i
 #endif
 
@@ -56,7 +55,7 @@
 typedef float vec##n[n]; \
 typedef union { \
 	vec##n v; \
-	_LINMATH_H_VECTOR_STRUCT(__VA_ARGS__); \
+	__LH_VECTOR_STRUCT(__VA_ARGS__); \
 } vec##n##_t; \
 static inline void vec##n##_dup(vec##n r, vec##n v) \
 { \
@@ -109,35 +108,60 @@ static inline void vec##n##_max(vec##n r, vec##n a, vec##n b) \
 { \
 	for(int i=0; i<n; ++i) \
 		r[i] = (a[i] > b[i]) ? a[i] : b[i]; \
-}
+} \
+extern void __THIS_REQUIRES_A_SEMICOLON(void)
 
-LINMATH_H_DEFINE_VEC(2, x, y)
-LINMATH_H_DEFINE_VEC(3, x, y, z)
-LINMATH_H_DEFINE_VEC(4, x, y, z, w)
+LINMATH_H_DEFINE_VEC(2, x, y);
+LINMATH_H_DEFINE_VEC(3, x, y, z);
+LINMATH_H_DEFINE_VEC(4, x, y, z, w);
 
-#ifdef LINMATH_H_STD_BUILDERS
-	//note: there's no macros for plain vectors constant initializers
-	//use default syntax for this purpose: {x,y} / {x,y,z} / {x,y,z,w}
+#ifndef LINMATH_H_NO_BUILDERS
+	/*
+	  NOTE: There's no macros for plain vectors constant initializers.
+	  Use default C syntax for this purpose:
+	    vec2 v_xy = {x,y};
+	    vec3 v_xyz = {x,y,z};
+	    vec4 v_xyzw = {x,y,z,w};
+	*/
 
-	//function arguments builders for plain vectors
-	#define VEC2(x,y)       (vec2){x,y}
-	#define VEC3(x,y,z)     (vec3){x,y,z}
-	#define VEC4(x,y,z,w)   (vec4){x,y,z,w}
+	/*
+	  Function arguments builders for plain vectors.
+	  Usage example:
+	    void f(vec2 v_xy);
+	    f(VEC2(1.0f, 2.0f));
+	*/
+	#define VEC2(x,y)     (vec2){x,y}
+	#define VEC3(x,y,z)   (vec3){x,y,z}
+	#define VEC4(x,y,z,w) (vec4){x,y,z,w}
 
-	//union types constant initializers, for compile-time initialization
+	/*
+	  Union types constant initializers, for compile-time initialization.
+	  Usage example:
+	    vec2_t vt_xy = iVEC2T(1.0f, 2.0f);
+	*/
 	#define iVEC2T(x,y)     {.v={x,y}}
 	#define iVEC3T(x,y,z)   {.v={x,y,z}}
 	#define iVEC4T(x,y,z,w) {.v={x,y,z,w}}
 
-	//function arguments builders for union types
-	#define VEC2T(x,y)      (vec2_t)iVEC2T(x,y)
-	#define VEC3T(x,y,z)    (vec3_t)iVEC3T(x,y,z)
-	#define VEC4T(x,y,z,w)  (vec4_t)iVEC4T(x,y,z,w)
+	/*
+	  Function arguments builders for union types.
+	  Usage example:
+	    void f(vec2_t vt_xy);
+	    f(VEC2T(1.0f, 2.0f));
+	*/
+	#define VEC2T(x,y)     (vec2_t)iVEC2T(x,y)
+	#define VEC3T(x,y,z)   (vec3_t)iVEC3T(x,y,z)
+	#define VEC4T(x,y,z,w) (vec4_t)iVEC4T(x,y,z,w)
 
-	//macros for casting plain vectors to union types
-	#define cVEC2T(v2)      VEC2T(v2[0],v2[1])
-	#define cVEC3T(v3)      VEC3T(v3[0],v3[1],v3[2])
-	#define cVEC4T(v4)      VEC4T(v4[0],v4[1],v4[2],v4[3])
+	/*
+	  Macros for casting plain vectors to union types.
+	  Usage example:
+	    vec2 v_xy = {1.0f, 2.0f};
+	    vec2_t vt_xy = cVEC2T(v_xy);
+	*/
+	#define cVEC2T(v2) VEC2T(v2[0],v2[1])
+	#define cVEC3T(v3) VEC3T(v3[0],v3[1],v3[2])
+	#define cVEC4T(v4) VEC4T(v4[0],v4[1],v4[2],v4[3])
 #endif
 
 static inline void vec3_mul_cross(vec3 r, vec3 a, vec3 b)
@@ -170,22 +194,22 @@ static inline void mat4x4_dup(mat4x4 M, mat4x4 N)
 static inline void mat4x4_row(vec4 r, mat4x4 M, int i)
 {
 	for(int k=0; k<4; ++k)
-		r[k] = M4E(M,k,i);
+		r[k] = __LH_M4E(M,k,i);
 }
 static inline void mat4x4_col(vec4 r, mat4x4 M, int i)
 {
 	for(int k=0; k<4; ++k)
-		r[k] = M4E(M,i,k);
+		r[k] = __LH_M4E(M,i,k);
 }
 static inline void mat4x4_set_row(mat4x4 M, vec4 v, int i)
 {
 	for(int k=0; k<4; ++k)
-		M4E(M,k,i) = v[k];
+		__LH_M4E(M,k,i) = v[k];
 }
 static inline void mat4x4_set_col(mat4x4 M, vec4 v, int i)
 {
 	for(int k=0; k<4; ++k)
-		M4E(M,i,k) = v[k];
+		__LH_M4E(M,i,k) = v[k];
 }
 static inline void mat4x4_transpose(mat4x4 M, mat4x4 N)
 {
@@ -224,15 +248,15 @@ static inline void mat4x4_scale_aniso(mat4x4 M, mat4x4 a, float x, float y, floa
 	mat4x4_set_col(M, a2, 2);
 
 	for(int i=0; i<4; ++i)
-		M4E(M,3,i) = M4E(a,3,i);
+		__LH_M4E(M,3,i) = __LH_M4E(a,3,i);
 }
 static inline void mat4x4_mul(mat4x4 M, mat4x4 a, mat4x4 b)
 {
 	mat4x4 temp;
 	for(int c=0; c<4; ++c) for(int r=0; r<4; ++r) {
-		M4E(temp,c,r) = 0.f;
+		__LH_M4E(temp,c,r) = 0.f;
 		for(int k=0; k<4; ++k)
-			M4E(temp,c,r) += M4E(a,k,r) * M4E(b,c,k);
+			__LH_M4E(temp,c,r) += __LH_M4E(a,k,r) * __LH_M4E(b,c,k);
 	}
 	mat4x4_dup(M, temp);
 }
@@ -241,15 +265,15 @@ static inline void mat4x4_mul_vec4(vec4 r, mat4x4 M, vec4 v)
 	for(int j=0; j<4; ++j) {
 		r[j] = 0.f;
 		for(int i=0; i<4; ++i)
-			r[j] += M4E(M,i,j) * v[i];
+			r[j] += __LH_M4E(M,i,j) * v[i];
 	}
 }
 static inline void mat4x4_translate(mat4x4 T, float x, float y, float z)
 {
 	mat4x4_identity(T);
-	M4E(T,3,0) = x;
-	M4E(T,3,1) = y;
-	M4E(T,3,2) = z;
+	__LH_M4E(T,3,0) = x;
+	__LH_M4E(T,3,1) = y;
+	__LH_M4E(T,3,2) = z;
 }
 static inline void mat4x4_translate_in_place(mat4x4 M, float x, float y, float z)
 {
@@ -257,13 +281,13 @@ static inline void mat4x4_translate_in_place(mat4x4 M, float x, float y, float z
 	vec4 r;
 	for(int i=0; i<4; ++i) {
 		mat4x4_row(r, M, i);
-		M4E(M,3,i) += vec4_mul_inner(r, t);
+		__LH_M4E(M,3,i) += vec4_mul_inner(r, t);
 	}
 }
 static inline void mat4x4_from_vec3_mul_outer(mat4x4 M, vec3 a, vec3 b)
 {
 	for(int i=0; i<4; ++i) for(int j=0; j<4; ++j)
-		M4E(M,i,j) = ((i<3) && (j<3)) ? (a[i] * b[j]) : 0.f;
+		__LH_M4E(M,i,j) = ((i<3) && (j<3)) ? (a[i] * b[j]) : 0.f;
 }
 static inline void mat4x4_rotate(mat4x4 R, mat4x4 M, float x, float y, float z, float angle)
 {
@@ -277,10 +301,10 @@ static inline void mat4x4_rotate(mat4x4 R, mat4x4 M, float x, float y, float z, 
 		mat4x4_from_vec3_mul_outer(T, u, u);
 
 		mat4x4 S = {
-			{       0.f, POS(u[2]), NEG(u[1]), 0.f },
-			{ NEG(u[2]),       0.f, POS(u[0]), 0.f },
-			{ POS(u[1]), NEG(u[0]),       0.f, 0.f },
-			{       0.f,       0.f,       0.f, 0.f }
+			{            0.f, __LH_POS(u[2]), __LH_NEG(u[1]), 0.f },
+			{ __LH_NEG(u[2]),            0.f, __LH_POS(u[0]), 0.f },
+			{ __LH_POS(u[1]), __LH_NEG(u[0]),            0.f, 0.f },
+			{            0.f,            0.f,            0.f, 0.f }
 		};
 
 		mat4x4_scale(S, S, s);
@@ -305,10 +329,10 @@ static inline void mat4x4_rotate_X(mat4x4 Q, mat4x4 M, float angle)
 	float s = sinf(angle);
 	float c = cosf(angle);
 	mat4x4 R = {
-		{ 1.f,    0.f,    0.f, 0.f },
-		{ 0.f,      c, POS(s), 0.f },
-		{ 0.f, NEG(s),      c, 0.f },
-		{ 0.f,    0.f,    0.f, 1.f }
+		{ 1.f,         0.f,         0.f, 0.f },
+		{ 0.f,           c, __LH_POS(s), 0.f },
+		{ 0.f, __LH_NEG(s),           c, 0.f },
+		{ 0.f,         0.f,         0.f, 1.f }
 	};
 	mat4x4_mul(Q, M, R);
 }
@@ -317,10 +341,10 @@ static inline void mat4x4_rotate_Y(mat4x4 Q, mat4x4 M, float angle)
 	float s = sinf(angle);
 	float c = cosf(angle);
 	mat4x4 R = {
-		{      c, 0.f, POS(s), 0.f },
-		{    0.f, 1.f,    0.f, 0.f },
-		{ NEG(s), 0.f,      c, 0.f },
-		{    0.f, 0.f,    0.f, 1.f }
+		{           c, 0.f, __LH_POS(s), 0.f },
+		{         0.f, 1.f,         0.f, 0.f },
+		{ __LH_NEG(s), 0.f,           c, 0.f },
+		{         0.f, 0.f,         0.f, 1.f }
 	};
 	mat4x4_mul(Q, M, R);
 }
@@ -329,10 +353,10 @@ static inline void mat4x4_rotate_Z(mat4x4 Q, mat4x4 M, float angle)
 	float s = sinf(angle);
 	float c = cosf(angle);
 	mat4x4 R = {
-		{      c, POS(s), 0.f, 0.f },
-		{ NEG(s),      c, 0.f, 0.f },
-		{    0.f,    0.f, 1.f, 0.f },
-		{    0.f,    0.f, 0.f, 1.f }
+		{           c, __LH_POS(s), 0.f, 0.f },
+		{ __LH_NEG(s),           c, 0.f, 0.f },
+		{         0.f,         0.f, 1.f, 0.f },
+		{         0.f,         0.f, 0.f, 1.f }
 	};
 	mat4x4_mul(Q, M, R);
 }
@@ -413,70 +437,76 @@ static inline void mat4x4_orthonormalize(mat4x4 R, mat4x4 M)
 
 static inline void mat4x4_frustum(mat4x4 M, float l, float r, float b, float t, float n, float f)
 {
-	M4E(M,0,0) = 2.f*n/(r-l);
-	M4E(M,0,1) = M4E(M,0,2) = M4E(M,0,3) = 0.f;
+	__LH_M4E(M,0,0) = 2.f*n/(r-l);
+	__LH_M4E(M,0,1) = __LH_M4E(M,0,2) = __LH_M4E(M,0,3) = 0.f;
 
-	M4E(M,1,1) = 2.f*n/(t-b);
-	M4E(M,1,0) = M4E(M,1,2) = M4E(M,1,3) = 0.f;
+	__LH_M4E(M,1,1) = 2.f*n/(t-b);
+	__LH_M4E(M,1,0) = __LH_M4E(M,1,2) = __LH_M4E(M,1,3) = 0.f;
 
-	M4E(M,2,0) = (r+l)/(r-l);
-	M4E(M,2,1) = (t+b)/(t-b);
-	M4E(M,2,2) = -(f+n)/(f-n);
-	M4E(M,2,3) = -1.f;
+	__LH_M4E(M,2,0) = (r+l)/(r-l);
+	__LH_M4E(M,2,1) = (t+b)/(t-b);
+	__LH_M4E(M,2,2) = -(f+n)/(f-n);
+	__LH_M4E(M,2,3) = -1.f;
 
-	M4E(M,3,2) = -2.f*(f*n)/(f-n);
-	M4E(M,3,0) = M4E(M,3,1) = M4E(M,3,3) = 0.f;
+	__LH_M4E(M,3,2) = -2.f*(f*n)/(f-n);
+	__LH_M4E(M,3,0) = __LH_M4E(M,3,1) = __LH_M4E(M,3,3) = 0.f;
 }
 static inline void mat4x4_ortho(mat4x4 M, float l, float r, float b, float t, float n, float f)
 {
-	M4E(M,0,0) = 2.f/(r-l);
-	M4E(M,0,1) = M4E(M,0,2) = M4E(M,0,3) = 0.f;
+	__LH_M4E(M,0,0) = 2.f/(r-l);
+	__LH_M4E(M,0,1) = __LH_M4E(M,0,2) = __LH_M4E(M,0,3) = 0.f;
 
-	M4E(M,1,1) = 2.f/(t-b);
-	M4E(M,1,0) = M4E(M,1,2) = M4E(M,1,3) = 0.f;
+	__LH_M4E(M,1,1) = 2.f/(t-b);
+	__LH_M4E(M,1,0) = __LH_M4E(M,1,2) = __LH_M4E(M,1,3) = 0.f;
 
-	M4E(M,2,2) = -2.f/(f-n);
-	M4E(M,2,0) = M4E(M,2,1) = M4E(M,2,3) = 0.f;
+	__LH_M4E(M,2,2) = -2.f/(f-n);
+	__LH_M4E(M,2,0) = __LH_M4E(M,2,1) = __LH_M4E(M,2,3) = 0.f;
 
-	M4E(M,3,0) = -(r+l)/(r-l);
-	M4E(M,3,1) = -(t+b)/(t-b);
-	M4E(M,3,2) = -(f+n)/(f-n);
-	M4E(M,3,3) = 1.f;
+	__LH_M4E(M,3,0) = -(r+l)/(r-l);
+	__LH_M4E(M,3,1) = -(t+b)/(t-b);
+	__LH_M4E(M,3,2) = -(f+n)/(f-n);
+	__LH_M4E(M,3,3) = 1.f;
 }
 static inline void mat4x4_perspective(mat4x4 m, float y_fov, float aspect, float n, float f)
 {
-	/* NOTE: Degrees are an unhandy unit to work with.
-	 * linmath.h uses radians for everything! */
+	/*
+	  NOTE: Degrees are an unhandy unit to work with.
+	  linmath.h uses radians for everything!
+	*/
+
 	float const a = 1.f / tanf(y_fov / 2.f);
 
-	M4E(m,0,0) = a / aspect;
-	M4E(m,0,1) = 0.f;
-	M4E(m,0,2) = 0.f;
-	M4E(m,0,3) = 0.f;
+	__LH_M4E(m,0,0) = a / aspect;
+	__LH_M4E(m,0,1) = 0.f;
+	__LH_M4E(m,0,2) = 0.f;
+	__LH_M4E(m,0,3) = 0.f;
 
-	M4E(m,1,0) = 0.f;
-	M4E(m,1,1) = a;
-	M4E(m,1,2) = 0.f;
-	M4E(m,1,3) = 0.f;
+	__LH_M4E(m,1,0) = 0.f;
+	__LH_M4E(m,1,1) = a;
+	__LH_M4E(m,1,2) = 0.f;
+	__LH_M4E(m,1,3) = 0.f;
 
-	M4E(m,2,0) = 0.f;
-	M4E(m,2,1) = 0.f;
-	M4E(m,2,2) = -((f+n)/(f-n));
-	M4E(m,2,3) = -1.f;
+	__LH_M4E(m,2,0) = 0.f;
+	__LH_M4E(m,2,1) = 0.f;
+	__LH_M4E(m,2,2) = -((f+n)/(f-n));
+	__LH_M4E(m,2,3) = -1.f;
 
-	M4E(m,3,0) = 0.f;
-	M4E(m,3,1) = 0.f;
-	M4E(m,3,2) = -((2.f*f*n)/(f-n));
-	M4E(m,3,3) = 0.f;
+	__LH_M4E(m,3,0) = 0.f;
+	__LH_M4E(m,3,1) = 0.f;
+	__LH_M4E(m,3,2) = -((2.f*f*n)/(f-n));
+	__LH_M4E(m,3,3) = 0.f;
 }
 static inline void mat4x4_look_at(mat4x4 m, vec3 eye, vec3 center, vec3 up)
 {
-	/* Adapted from Android's OpenGL Matrix.java.                        */
-	/* See the OpenGL GLUT documentation for gluLookAt for a description */
-	/* of the algorithm. We implement it in a straightforward way:       */
+	/*
+	  Adapted from Android's OpenGL Matrix.java.
+	  See the OpenGL GLUT documentation for gluLookAt for a description
+	  of the algorithm. We implement it in a straightforward way.
 
-	/* TODO: The negation of vector can be spared by swapping the order of
-	 *       operands in the following cross products in the right way.  */
+	  TODO: The negation of vector can be spared by swapping the order of
+	  operands in the following cross products in the right way.
+	*/
+
 	vec3 f;
 	vec3_sub(f, center, eye);
 	vec3_norm(f, f);
@@ -488,25 +518,25 @@ static inline void mat4x4_look_at(mat4x4 m, vec3 eye, vec3 center, vec3 up)
 	vec3 t;
 	vec3_mul_cross(t, s, f);
 
-	M4E(m,0,0) =  s[0];
-	M4E(m,0,1) =  t[0];
-	M4E(m,0,2) = -f[0];
-	M4E(m,0,3) =  0.f;
+	__LH_M4E(m,0,0) =  s[0];
+	__LH_M4E(m,0,1) =  t[0];
+	__LH_M4E(m,0,2) = -f[0];
+	__LH_M4E(m,0,3) =  0.f;
 
-	M4E(m,1,0) =  s[1];
-	M4E(m,1,1) =  t[1];
-	M4E(m,1,2) = -f[1];
-	M4E(m,1,3) =  0.f;
+	__LH_M4E(m,1,0) =  s[1];
+	__LH_M4E(m,1,1) =  t[1];
+	__LH_M4E(m,1,2) = -f[1];
+	__LH_M4E(m,1,3) =  0.f;
 
-	M4E(m,2,0) =  s[2];
-	M4E(m,2,1) =  t[2];
-	M4E(m,2,2) = -f[2];
-	M4E(m,2,3) =  0.f;
+	__LH_M4E(m,2,0) =  s[2];
+	__LH_M4E(m,2,1) =  t[2];
+	__LH_M4E(m,2,2) = -f[2];
+	__LH_M4E(m,2,3) =  0.f;
 
-	M4E(m,3,0) =  0.f;
-	M4E(m,3,1) =  0.f;
-	M4E(m,3,2) =  0.f;
-	M4E(m,3,3) =  1.f;
+	__LH_M4E(m,3,0) =  0.f;
+	__LH_M4E(m,3,1) =  0.f;
+	__LH_M4E(m,3,2) =  0.f;
+	__LH_M4E(m,3,3) =  1.f;
 
 	mat4x4_translate_in_place(m, -eye[0], -eye[1], -eye[2]);
 }
@@ -566,9 +596,9 @@ static inline void quat_rotate(quat r, float angle, vec3 axis) {
 static inline void quat_mul_vec3(vec3 r, quat q, vec3 v)
 {
 	/*
-	* Method by Fabian 'ryg' Giessen (of Farbrausch)
-		t = 2 * cross(q.xyz, v)
-		v' = v + q.w * t + cross(q.xyz, t)
+	  Method by Fabian 'ryg' Giessen (of Farbrausch):
+	    t = 2 * cross(q.xyz, v)
+	    v' = v + q.w * t + cross(q.xyz, t)
 	*/
 	vec3 t;
 	vec3 q_xyz = {q[0], q[1], q[2]};
@@ -594,29 +624,32 @@ static inline void mat4x4_from_quat(mat4x4 M, quat q)
 	float c2 = c*c;
 	float d2 = d*d;
 
-	M4E(M,0,0) = a2 + b2 - c2 - d2;
-	M4E(M,0,1) = 2.f*(b*c + a*d);
-	M4E(M,0,2) = 2.f*(b*d - a*c);
-	M4E(M,0,3) = 0.f;
+	__LH_M4E(M,0,0) = a2 + b2 - c2 - d2;
+	__LH_M4E(M,0,1) = 2.f*(b*c + a*d);
+	__LH_M4E(M,0,2) = 2.f*(b*d - a*c);
+	__LH_M4E(M,0,3) = 0.f;
 
-	M4E(M,1,0) = 2.f*(b*c - a*d);
-	M4E(M,1,1) = a2 - b2 + c2 - d2;
-	M4E(M,1,2) = 2.f*(c*d + a*b);
-	M4E(M,1,3) = 0.f;
+	__LH_M4E(M,1,0) = 2.f*(b*c - a*d);
+	__LH_M4E(M,1,1) = a2 - b2 + c2 - d2;
+	__LH_M4E(M,1,2) = 2.f*(c*d + a*b);
+	__LH_M4E(M,1,3) = 0.f;
 
-	M4E(M,2,0) = 2.f*(b*d + a*c);
-	M4E(M,2,1) = 2.f*(c*d - a*b);
-	M4E(M,2,2) = a2 - b2 - c2 + d2;
-	M4E(M,2,3) = 0.f;
+	__LH_M4E(M,2,0) = 2.f*(b*d + a*c);
+	__LH_M4E(M,2,1) = 2.f*(c*d - a*b);
+	__LH_M4E(M,2,2) = a2 - b2 - c2 + d2;
+	__LH_M4E(M,2,3) = 0.f;
 
-	M4E(M,3,0) = M4E(M,3,1) = M4E(M,3,2) = 0.f;
-	M4E(M,3,3) = 1.f;
+	__LH_M4E(M,3,0) = __LH_M4E(M,3,1) = __LH_M4E(M,3,2) = 0.f;
+	__LH_M4E(M,3,3) = 1.f;
 }
 
 static inline void mat4x4o_mul_quat(mat4x4 R, mat4x4 M, quat q)
 {
-	/*  XXX: The way this is written only works for orthogonal matrices. */
-	/* TODO: Take care of non-orthogonal case. */
+	/*
+	  NOTE: The way this is written only works for orthogonal matrices.
+	  TODO: Take care of non-orthogonal case.
+	*/
+
 	vec4 m0, m1, m2;
 	mat4x4_col(m0, M, 0);
 	mat4x4_col(m1, M, 1);
@@ -630,8 +663,8 @@ static inline void mat4x4o_mul_quat(mat4x4 R, mat4x4 M, quat q)
 	mat4x4_set_col(R, r0, 0);
 	mat4x4_set_col(R, r1, 1);
 	mat4x4_set_col(R, r2, 2);
-	M4E(R,3,0) = M4E(R,3,1) = M4E(R,3,2) = 0.f;
-	M4E(R,3,3) = 1.f;
+	__LH_M4E(R,3,0) = __LH_M4E(R,3,1) = __LH_M4E(R,3,2) = 0.f;
+	__LH_M4E(R,3,3) = 1.f;
 }
 static inline void quat_from_mat4x4(quat q, mat4x4 M)
 {
@@ -656,13 +689,13 @@ static inline void quat_from_mat4x4(quat q, mat4x4 M)
 	}
 
 	q[0] = r/2.f;
-	q[1] = ( M4E(M,p[0],p[1]) - M4E(M,p[1],p[0]) )/(2.f*r);
-	q[2] = ( M4E(M,p[2],p[0]) - M4E(M,p[0],p[2]) )/(2.f*r);
-	q[3] = ( M4E(M,p[2],p[1]) - M4E(M,p[1],p[2]) )/(2.f*r);
+	q[1] = ( __LH_M4E(M,p[0],p[1]) - __LH_M4E(M,p[1],p[0]) )/(2.f*r);
+	q[2] = ( __LH_M4E(M,p[2],p[0]) - __LH_M4E(M,p[0],p[2]) )/(2.f*r);
+	q[3] = ( __LH_M4E(M,p[2],p[1]) - __LH_M4E(M,p[1],p[2]) )/(2.f*r);
 }
 
-#undef M4E
-#undef POS
-#undef NEG
+#undef __LH_M4E
+#undef __LH_POS
+#undef __LH_NEG
 
 #endif // LINMATH_H
